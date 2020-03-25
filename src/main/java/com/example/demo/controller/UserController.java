64cc2c1e3f30021a04a6db5878a47acc8d026466
Alejandro Lozano <alex93lzn@gmail.com>
2020-03-25 18:46:32 +0100

    controller user

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.interfaces.IUsuarioSERVICE;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	
+	@Autowired
+	IUsuarioSERVICE userservice;
+
+}
