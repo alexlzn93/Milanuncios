c937ad3bddd2813ff711dabd7ea4700a63c120a5
Alejandro Lozano <alex93lzn@gmail.com>
2020-04-05 12:26:04 +0200

    Anotacion @Repository en los DAO

--- src/main/java/com/example/demo/dao/IAnuncioRepository.java
+++ src/main/java/com/example/demo/dao/IAnuncioRepository.java
@@ -6,9 +6,10 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.Anuncio;
-
+@Repository
 public interface IAnuncioRepository extends CrudRepository<Anuncio, Integer> {
 
 	//EJEMPLOS DE QUERY METHODS
--- src/main/java/com/example/demo/dao/IUsuarioRepository.java
+++ src/main/java/com/example/demo/dao/IUsuarioRepository.java
@@ -1,11 +1,13 @@
 package com.example.demo.dao;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.Usuario;
 
 //DAO
 //CrudRepository<Beans,tipo de dato del campo clave(user)>
+@Repository
 public interface IUsuarioRepository extends CrudRepository<Usuario, String> { 
 																				 
 }
